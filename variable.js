fe0b1f07dd928ea4ebd63925a4c6ee0bb12b7789
husnain585 <husnain7t9@gmail.com>
2024-10-09 19:42:13 +0500

    let and const

--- variable.js
+++ variable.js
@@ -4,6 +4,28 @@ const var1 = 1;
 console.log(var1);
 {
     const var1 = 2;
-    console.log(var2);
+    console.log(var1);
 }
 console.log(var1);
+
+// let variable
+{
+
+    let var2 = 1;
+    console.log(var2);
+    {
+        let var2 = 2;
+        console.log(var2);
+    }
+    console.log(var2);
+}
+
+// var variable
+
+var var3 = 1;
+console.log(var3);
+{
+    var var3 = 2;
+    console.log(var3);
+}
+console.log(var3);
